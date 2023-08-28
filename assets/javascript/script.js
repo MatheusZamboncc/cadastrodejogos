c9240e63404c59fc19d527ee2a5350745e74d915
matheuscosta <matheuszambon564@gmail.com>
2023-08-28 15:38:28 -0300

    feat: commit already fineshed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,14 @@
 function verificarInputs() {
-
     let titulo = document.getElementById("input-titulo").value;
     let preco = document.getElementById("input-preco").value;
     let descricao = document.getElementById("input-descricao").value;
     let imgLink = document.getElementById("input-imgLink").value;
     let plataforma = document.getElementById("input-plataforma").value;
-
     console.log(titulo);
     console.log(preco);
     console.log(descricao);
     console.log(imgLink);
     console.log(plataforma);
-
     if (titulo == "" || preco == "" || descricao == "" || imgLink == "" || plataforma == "") {
         envieMsg("preencha todos os campos", "erro");
         return true;
@@ -20,24 +17,20 @@ function verificarInputs() {
         envieMsg("jogo cadastrado", "sucesso");
         return false;
     }
-
 }
-
 function envieMsg(msg, tipoMsg) {
     let msgDiv = document.getElementById("msg");
-    msgDiv.innerHTML = "";
-
+    msgDiv.innerHTML = '';
     let msgParaTela = `
     <p class = "${tipoMsg}">${msg}</p>
     `
     msgDiv.innerHTML = msgParaTela;
-
     setTimeout(function () {
         msgDiv.innerHTML = '';
     }, 3000)
 }
 class Jogo {
-    constructor(titulo, preco, descricao, plataforma, imgLink) {
+    constructor(titulo, preco, descricao, plataforma, imgLink, id) {
         this.titulo = titulo;
         this.preco = preco;
         this.descricao = descricao;
@@ -50,29 +43,23 @@ class Jogo {
     }
 }
 
-
 function comporJogo() {
-
     let titulo = document.getElementById("input-titulo").value;
     let preco = document.getElementById("input-preco").value;
     let descricao = document.getElementById("input-descricao").value;
     let imgLink = document.getElementById("input-imgLink").value;
     let plataforma = document.getElementById("input-plataforma").value;
-
     const jogo = new Jogo(titulo, preco, descricao, plataforma,imgLink);
+    const id = jogo.generateID()
     console.log(jogo);
-
     BibliotecaJogos.adicionar(jogo);
-
     console.log(BibliotecaJogos);
     renderizarConteudo();
 }
-
 class ListaJogos {
     constructor() {
         this.ListaJogosArray = [];
     }
-
     adicionar(parametro){
         if  (verificarInputs()) {
             envieMsg("preencha todos os campos", "erro")
@@ -92,29 +79,21 @@ class ListaJogos {
 }
 const ListaTeste = new ListaJogos();
 // console.log(ListaTeste);
-
 const BibliotecaJogos = new ListaJogos();
 console.log(BibliotecaJogos);
-
 function limparImputs() {
-
     document.getElementById('input-titulo').value = "";
     document.getElementById('input-preco').value = "";
     document.getElementById('input-descricao').value = "";
     document.getElementById('input-imgLink').value = "";
     document.getElementById('input-plataforma').value = "";
-
 }
 console.clear();
-
 function renderizarConteudo() {
     const listaHTML = document.getElementById("gameList");
     listaHTML.innerHTML = "";
-
     let array = BibliotecaJogos.ListaJogosArray;
-
     array.forEach(jogo => {
-
         const jogoDiv = `
         <div class = 'jogoDetalhe'>
         <p>Titulo: ${jogo.titulo}</p>
@@ -128,16 +107,14 @@ function renderizarConteudo() {
         listaHTML.innerHTML += jogoDiv;
     });
 }
-
-
+function remover(id){
+    BibliotecaJogos.removerJogos(id)
+renderizarConteudo()
+}
 function isURLValida(url) {
     if(url.match(/\.(jpeg|jpg|gif|png)$/) != null){
         return true;
     } else {
         return false;
     }
-}
-
-function remover(id){
-    BibliotecaJogos.removerJogos(id)
 }
